78a24511b9bcac7355ee51ff8a5cce5bef6c6d8e
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2026-04-05 19:15:57 +0200

    client : ajout d'un faux test unitaire pour vérifier la pipeline

---
+++ client/tests/units/placholder.test.js
@@ -0,0 +1,15 @@
+describe("Placeholder", () => {
+
+    test("placholder test", async () => {
+        // Arrange
+        const a = 1;
+        const b = 2;
+
+        // Act
+        const c = a + b;
+
+        // Assert
+        expect(c).toBe(3);
+    });
+
+});
