fb262977f6849f2ce26b98fbe68968c021dbe305
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2026-04-14 00:09:44 +0200

    backend : conversion js en ts

---
+++ backend/src/routes/getGreeting.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+const GREETING = "Hello world";
+
+const getGreeting = async (req: Request, res: Response): Promise<void> => {
+  res.send({
+    greeting: GREETING,
+  });
+};
+
+export default getGreeting;
